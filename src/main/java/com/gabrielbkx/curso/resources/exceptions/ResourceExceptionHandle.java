ee58c7a87acec79329b88a150637c5da70f39801
Gabriel <oliveirafrerreira97@hotmail.com>
2025-01-13 23:50:20 -0300

    update exception handle

---
+++ src/main/java/com/gabrielbkx/curso/resources/exceptions/ResourceExceptionHandle.java
@@ -0,0 +1,34 @@
+package com.gabrielbkx.curso.resources.exceptions;
+
+import com.gabrielbkx.curso.services.exceptions.DataBaseException;
+import com.gabrielbkx.curso.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandle {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<StandardError> database(DataBaseException e, HttpServletRequest request) {
+
+        String error = "DataBase error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(),status.value(),error,e.getMessage(),request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+
+}
--- src/main/java/com/gabrielbkx/curso/services/UserService.java
+++ src/main/java/com/gabrielbkx/curso/services/UserService.java
@@ -2,6 +2,8 @@ package com.gabrielbkx.curso.services;
 
 import com.gabrielbkx.curso.entities.User;
 import com.gabrielbkx.curso.repositories.UserRepository;
+import com.gabrielbkx.curso.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,21 +23,32 @@ public class UserService {
     public User findById(Long id) {
 
         Optional<User> user = repository.findById(id);
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
         return repository.save(obj);
     }
 
-    public void delete(Long id){
-         repository.deleteById(id);
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+
+        }catch(RuntimeException e) {
+            e.printStackTrace();
+
+        }
     }
 
     public User update(long id,User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity,obj);
-        return repository.save(entity);
+       try{
+           User entity = repository.getReferenceById(id);
+           updateData(entity,obj);
+           return repository.save(entity);
+
+       }catch(EntityNotFoundException e) {
+           throw new ResourceNotFoundException(id);
+       }
     }
 
     private void updateData(User entity, User obj) {
